cb8e6b2e95f14d0df670961328a51a355dbd3acd
Jack Kamm <jackkamm@gmail.com>
2018-03-27 08:16:40 +0100

    Nikola auto commit.
    Nikola version: 7.8.3

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
-        ("/blog", "Blog"),
+        #("/blog", "Blog"),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed"),
